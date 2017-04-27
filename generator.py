2131a0581698385f95d4e3c39a2da60b09e6fa16
Jack "Chubs" Clark <clark.jack554@gmail.com>
2017-04-26 21:46:56 -0500

    Created Generator
    This is going to be the place where all of the magic is going to happen. Trust me, it's going to be great! Believe me! *totally not a Trump reference*

---
+++ generator.py
@@ -0,0 +1,13 @@
+# Code written by Jack Clark
+# Copyright 2017(c) the G.N.U. General Public License
+# This code takes in a webpage and some information, and then it generates a chrome extension that you can add
+# your browser very seamlessly
+import os
+import sys
+import platform
+
+# Class to store all of the information for the generator
+class Extension(name):
+	self.name = name
+	def generateJSON(name):
+
