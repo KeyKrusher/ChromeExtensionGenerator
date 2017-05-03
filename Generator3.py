ce85e6c2a21efd8c09b46619c03a88d9e606748c
Jack "Chubs" Clark <clark.jack554@gmail.com>
2017-05-03 09:36:52 -0500

    Update Generator3.py
    Just going to test out different ways to generate the script. Once again, the Python 2 version will be out soon afterwards as well.

--- Generator3.py
+++ Generator3.py
@@ -10,6 +10,29 @@ import sys
 import platform
 import json
 
+_Extension = {
+  "manifest_version": 2,
+
+  "name": "Place Extension Name Here",
+  "description": "Place description here.",
+  "version": "Place version here",
+
+  "browser_action": {
+    "default_icon": "icon.png",
+    "default_popup": "popup.html"
+  },
+  "permissions": [
+    "activeTab",
+    "https://ajax.googleapis.com/"
+  ]
+}
+{
+  "browser_action": {
+    "default_icon": "icon.png",
+    "default_popup": "popup.html",
+    "default_title": "Click here!"
+  },
+}
 def getExtensionInformation():
 	extension_name = input("Extension name:")
 	extension_description = input("Extension description:")
@@ -17,14 +40,44 @@ def getExtensionInformation():
 	extension_icon = input("Icon:")
 	extension_popup = input("Popup:")
 
-	UserExtension = Extension(extension_name, extension_description, extension_verion, extension_icon, extension_popup)
+	UserExtension = Extension(extension_name, extension_description, extension_version, extension_icon, extension_popup)
 
 class Extension(extension_name, extension_description, extension_version, extension_icon, extension_popup):
+	Extension_ = {
+  		"manifest_version": 2,
+  		"name": _name,
+  		"description": _description,
+  		"version": _version,
+  	"browser_action": {
+    	"default_icon": _icon,
+    	"default_popup": _popup
+  	},
+  	"permissions": [
+    	"activeTab",
+    	"https://ajax.googleapis.com/"
+  	]
+	}
+	{
+  	"browser_action": {
+    	"default_icon": "icon.png",
+    	"default_popup": "popup.html",
+    	"default_title": "Click here!"
+  	},
+	}
+	def generate(extension_to_make):
+		# Dump the JSON
+		json.dumps(extension_to_make)
 	def __init__(self, extension_name, extension_description, extension_version, extension_popup, extension_icon):
-		self.extension_name = extension_name
-		self.extension_description = extension_description
-		self.extension_version = extension_version
-		self.extension_popup = extension_popup
-		self.extension_icon = extension_icon
+		self._name = extension_name
+		self._description = extension_description
+		self._version = extension_version
+		self._popup = extension_popup
+		self._icon = extension_icon
+#    _extension_name = json.loads('{"name": "Place Extension Name Here"')
+#    _extension_description = json.loads('{"description": "Place description here."}')
+#    _extension_version = json.loads('{"version": "Place version here"}')
+#    _extension_icon = json.loads('{"": ""}')
+#    _extension_popup = json.loads('{"": ""}')
+    
 
 getExtensionInformation()
