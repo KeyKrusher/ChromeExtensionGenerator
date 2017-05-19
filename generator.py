84c7b2cf583b41edebd7e1081754e0e74eee0515
Jack "Chubs" Clark <clark.jack554@gmail.com>
2017-05-19 09:47:46 -0500

    Updated Readme

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 # Code written by Jack Clark
 # Copyright 2017(c) the G.N.U. General Public License
 # This code takes in a webpage and some information, and then it generates a chrome extension that you can add
-# your browser very seamlessly
+# your browser very seamlessly. It also helps you resize and change currently existing extensions.
 import os
 import sys
 import platform
